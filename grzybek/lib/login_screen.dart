f1c5cfd3f6026ac2a6da5d84cc845497bf088422
Patryk Kostecki <patrykkos12@gmail.com>
2024-03-05 13:17:48 +0100

    Drugi ekran

--- grzybek/lib/login_screen.dart
+++ grzybek/lib/login_screen.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Second Screen'),
-      ),
-      body: Center(
-        child: Text('LOGOWANIE fdsgsdgdfhfdhdhdKUREWNE.'),
-      ),
-    );
+    return MaterialApp(
+        home: Builder(
+      builder: (context) => Scaffold(
+          body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+            child: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+              SvgPicture.asset(
+                'assets/LOGO.svg',
+                width: 256,
+                height: 256,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "WITAJ!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 18.0, vertical: 0.1),
+                child: Text(
+                  "Zaloguj się do aplikacji Grzybek aby mieć dostęp do wiekszych mozliwosci!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.brown),
+                ),
+                onPressed: () {},
+                child: Text('Logowanie'),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.brown),
+                ),
+                onPressed: () {},
+                child: Text('Rejestracja'),
+              ),
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                onPressed: () {},
+                child: Text('Kontunuuj bez logowania'),
+              )
+            ]))),
+      )),
+    ));
   }
 }
